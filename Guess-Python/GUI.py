71da238c1f3a5ccf3b5e1a9978bdbfccbd07f25f
dhalcin <hankgit-h@hotmail.com>
2023-06-29 16:08:55 -0500

    Update

--- Guess-Python/GUI.py
+++ Guess-Python/GUI.py
@@ -1,10 +1,17 @@
 from tkinter import *
 from tkinter import ttk
 
-root = Tk()
-root.title("Guess")
-frm = ttk.Frame(root, padding=100)
-frm.grid()
-ttk.Label(frm, text="Guess-Python").grid(column=0, row=20)
 
-root.mainloop()
+def main():
+
+    root = Tk()
+    root.title("Guess")
+    frm = ttk.Frame(root, padding=100)
+    frm.grid()
+    ttk.Label(frm, text="Guess-Python").grid(column=0, row=20)
+
+    root.mainloop()
+
+
+if __name__ == "__main__":
+    main()
